2e790b711ad1abea9c1a006b3a5910e82aa6ae1f
HouseA02 <ADAM.HOUSE21@bathspa.ac.uk>
2024-03-18 03:28:07 +0000

    Revert "Revert "a""
    This reverts commit 693c628b88df990033978966e93ce5bf6ef11990.

--- DiceGame/Assets/Scripts/DialogueManager.cs
+++ DiceGame/Assets/Scripts/DialogueManager.cs
@@ -1,23 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    [SerializeField]
+    private TMP_Text nameBox;
+    [SerializeField]
+    private TMP_Text textBox;
+    [SerializeField]
+    private Transform battleArenaCam;
 
+    private string speaker, text;
+    private GameObject speakerObject;
+    private GameObject textParent;
+
+    public void Initiate(string line)
+    {
+        var half = line.Split(':');
+        speaker = half[0];
+        text = half[1];
+        speakerObject = GameObject.Find(speaker);
+        textParent = textBox.transform.parent.gameObject;
+        textParent.SetActive(true);
+        textParent.transform.LookAt(battleArenaCam);
+        textBox.text = text;
+        nameBox.text = speaker;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void EndDialogue()
     {
-
-    }
-
-    public void Initiate(GameObject speaker, string text)
-    {
-
+        textParent.SetActive(false);
     }
 }
